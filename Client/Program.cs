9d0c1b0929e00d21ec95a7a8d73e6c750388135d
sid24desai <104271822+sid24desai@users.noreply.github.com>
2024-05-22 12:56:14 +0530

    Update Program.cs

--- Client/Program.cs
+++ Client/Program.cs
@@ -9,3 +9,4 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
